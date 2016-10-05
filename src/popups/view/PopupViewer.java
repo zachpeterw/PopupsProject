16ed93d4dc9ffe03b74e2d56abf937c4cd033d18
zachpeterw <zachpeterw@gmail.com>
2016-10-05 16:43:08 -0600

    added jdock comments and popup methods in popups.view

--- src/popups/view/PopupViewer.java
+++ src/popups/view/PopupViewer.java
@@ -3,11 +3,22 @@ import javax.swing.JOptionPane;
 
 public class PopupViewer 
 {
-
+	/**
+	 * creates a popup to show the user a String.
+	 * @param message the String to show the user.
+	 */
+	
 	public void displayMessage(String message)
 	{
 		JOptionPane.showMessageDialog(null, message);
 	}
+	
+	/**
+	 * Creates a popup to ask a question to the user. Stores the response
+	 * as a String
+	 * @param question The question being asked to the user
+	 * @return The users response.
+	 */
 	
 	public String collectResponse(String question)
 	{
@@ -16,10 +27,16 @@ public class PopupViewer
 		return response;
 	}
 
-	public int collectOption(String question)
+	/**
+	 * Creates a popup to ask a yes/no/cancel type question
+	 * @param question The question being asked the user.
+	 * @returnThe constant value from JOptionPane for y/n/c
+	 */
+	
+	public int collectUserOption(String question)
 	{
-		int optionNumber = 0;
-		optionNumber = JOptionPane.showConfirmDialog(null,  question);
-		return optionNumber;
+		int response = 0;
+		response = JOptionPane.showConfirmDialog(null,  question);
+		return response;
 	}
 }
